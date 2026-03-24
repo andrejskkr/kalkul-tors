2f1087ce4d71ddeaa4b7ffbc0069bc8304c983ea
andrejskkr <andrejs.krasnovs@edu.kekava.lv>
2026-03-24 09:38:52 +0200

    Update kalkulators.py

---
+++ kalkulators.py
@@ -0,0 +1,163 @@
+from tkinter import*
+from math import*
+import tkinter as tk
+
+manslogs=Tk()
+manslogs.title('Kalkulators')
+
+
+
+def btnClick(number):
+    current=e.get() # nolasa esošo skaitli
+    e.delete(0,END) # nodzēš
+    newNumber=str(current)+str(number)
+    e.insert(0,newNumber) # ievieto displejā
+    return 0 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+btn0=Button(manslogs, text='0',padx='40',pady='20', command=lambda:btnClick(0))
+btn1=Button(manslogs, text='1',padx='40',pady='20', command=lambda:btnClick(1))
+btn2=Button(manslogs, text='2',padx='40',pady='20', command=lambda:btnClick(2))
+btn3=Button(manslogs, text='3',padx='40',pady='20', command=lambda:btnClick(3))
+btn4=Button(manslogs, text='4',padx='40',pady='20', command=lambda:btnClick(4))
+btn5=Button(manslogs, text='5',padx='40',pady='20', command=lambda:btnClick(5))
+btn6=Button(manslogs, text='6',padx='40',pady='20', command=lambda:btnClick(6))
+btn7=Button(manslogs, text='7',padx='40',pady='20', command=lambda:btnClick(7))
+btn8=Button(manslogs, text='8',padx='40',pady='20', command=lambda:btnClick(8))
+btn9=Button(manslogs, text='9',padx='40',pady='20', command=lambda:btnClick(9))
+btnSum=Button(manslogs, text='+',padx='40',pady='20')
+btnSub=Button(manslogs, text='-',padx='40',pady='20')
+btnequals=Button(manslogs, text='=',padx='40',pady='20')
+btnreiz=Button(manslogs, text='*',padx='40',pady='20')
+btndal=Button(manslogs, text='/',padx='40',pady='20')
+btnclear=Button(manslogs, text='C',padx='40',pady='20', fg="red")
+e=Entry(manslogs, width=15, bd=20, font=("Arial Black", 20))
+
+
+
+btn7.grid(row=1,column=0)
+btn8.grid(row=1,column=1)
+btn9.grid(row=1,column=2)
+btndal.grid(row=1,column=3)
+btn4.grid(row=2,column=0)
+btn5.grid(row=2,column=1)
+btn6.grid(row=2,column=2)
+btnreiz.grid(row=2,column=3)
+btn1.grid(row=3,column=0)
+btn2.grid(row=3,column=1)
+btn3.grid(row=3,column=2)
+btnSub.grid(row=3,column=3)
+btnclear.grid(row=4,column=0)
+btn0.grid(row=4,column=1)
+btnequals.grid(row=4,column=2)
+btnSum.grid(row=4,column=3)
+e.grid(row=0,column=0, columnspan=4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+manslogs.mainloop()
